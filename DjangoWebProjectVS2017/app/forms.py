ca38ff10b2f8b149dc5ea8be2d30c99f8c4517e3
asierbarrasa <asierjarrillero@gmail.com>
2018-05-10 13:15:45 +0200

    hola

---
+++ DjangoWebProjectVS2017/app/forms.py
@@ -0,0 +1,54 @@
+"""
+Definition of forms.
+"""
+
+from django import forms
+from app.models import Question,Choice,User
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import ugettext_lazy as _
+
+class FormPregunta (forms.ModelForm):
+
+        class Meta:
+            fields = ('question_text','choice_text','choice_text','choice_text',)
+
+class QuestionForm(forms.ModelForm):
+        tema = forms.CharField(widget=forms.TextInput(
+            attrs={
+                'class' : 'form-control',
+            }
+        ))
+
+        question_text = forms.CharField(widget=forms.TextInput(
+            attrs={
+                'class' : 'form-control',
+            }
+        ))
+
+        class Meta:
+            model = Question
+            fields = ('question_text','tema',)
+            
+
+class ChoiceForm(forms.ModelForm):
+
+        class Meta:
+            model = Choice
+            fields = ('choice_text',)
+
+class UserForm(forms.ModelForm):
+
+        class Meta:
+            model = User
+            fields = ('email','nombre',)
+
+class BootstrapAuthenticationForm(AuthenticationForm):
+    """Authentication form which uses boostrap CSS."""
+    username = forms.CharField(max_length=254,
+                               widget=forms.TextInput({
+                                   'class': 'form-control',
+                                   'placeholder': 'User name'}))
+    password = forms.CharField(label=_("Password"),
+                               widget=forms.PasswordInput({
+                                   'class': 'form-control',
+                                   'placeholder':'Password'}))
